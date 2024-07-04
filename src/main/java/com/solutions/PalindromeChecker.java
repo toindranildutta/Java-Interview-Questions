ec60d1fc8f60821194a910f58f8f296be08801ea
Indranil_Dutta <toindranildutta@gmail.com>
2024-07-04 15:26:30 +0530

    palindrome checker

---
+++ src/main/java/com/solutions/PalindromeChecker.java
@@ -0,0 +1,15 @@
+package com.solutions;
+
+/*
+ * String palindrome
+ * Program to verify whether a given string is a palindrome or not.
+ * A string is a palindrome if it is equal to the reverse of itself.
+ */
+public class PalindromeChecker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
