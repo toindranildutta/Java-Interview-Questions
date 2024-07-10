8033de218582717ebc893802f4665b2e90c2911c
Indranil_Dutta <toindranildutta@gmail.com>
2024-07-10 07:18:30 +0530

    Factorial Finder

---
+++ src/main/java/com/solutions/FactorialFinder.java
@@ -0,0 +1,14 @@
+package com.solutions;
+
+/*
+ * 7. Factorial
+ * Program to calculate and display the factorial of any given number.
+ */
+
+public class FactorialFinder {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
